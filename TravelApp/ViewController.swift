42ff7b53fa2e5f757a570890a3f244d942c678d9
User23 <user23@user23de-imac.local>
2019-01-09 10:50:13 +0800

    Initial Commit

---
+++ TravelApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TravelApp
+//
+//  Created by User23 on 2019/1/9.
+//  Copyright © 2019 Laura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
